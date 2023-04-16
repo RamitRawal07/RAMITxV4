49d3a87a0c29adab333700a68e7f36dcabfbdf1a
RamitRawal07 <130843145+RamitRawal07@users.noreply.github.com>
2023-04-16 17:00:36 +0545

    Delete RAMIT.py

--- RAMIT.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-RAMIT=platform.architecture()[0]
-if RAMIT=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif RAMIT=="64bit":
-    #print('Command is in update wait we will fix it soon !')
-    __import__("RAMIT")
